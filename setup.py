26066484a2a0f6ace510bdcf60725218658a6445
Daniel Harley Jordan <61107719+DanHarJor@users.noreply.github.com>
2025-10-27 21:50:27 +0200

    Add setup.py for package configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sg-lib",
+    version="0.1.0",
+    packages=find_packages(),  # This will include sg_lib and sg_lib.grid
+    description="Surrogate grid library",
+    author="Your Name",
+)
